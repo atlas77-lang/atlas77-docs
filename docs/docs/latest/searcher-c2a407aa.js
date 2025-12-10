1632f1d4c7b7bbee6a6568e2acd4ffdc2c19eb6f
Gipson62 <70176484+Gipson62@users.noreply.github.com>
2025-12-10 23:23:23 +0100

    fix: Output docs was wrong

--- docs/docs/latest/searcher-c2a407aa.js
+++ docs/docs/latest/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c063cbf4.js',
+                path_to_root + 'searchindex-4d90fe75.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
