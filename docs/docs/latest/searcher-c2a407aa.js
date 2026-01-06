5d07e7133f8e9f18cd15de733731ad53cb994201
Gipson62 <j.h.gipson62@gmail.com>
2026-01-06 23:30:24 +0100

    fix: Forgot to remove old items from std

--- docs/docs/latest/searcher-c2a407aa.js
+++ docs/docs/latest/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a2774121.js',
+                path_to_root + 'searchindex-1b3fb120.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
